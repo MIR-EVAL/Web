494032a64116b8484704b9aede2e3da48160312c
ekaterina.valova <e.valova@mail.ru>
2023-05-16 15:46:02 +0300

    Homework 4 tasks 1, 2

---
+++ my.sql
@@ -0,0 +1,13 @@
+-- create
+CREATE TABLE CLASSMATE (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age TEXT NOT NULL,
+address TEXT NOT NULL
+);
+-- insert
+INSERT INTO CLASSMATE VALUES (0001, 'Иван', '18', 'Москва');
+INSERT INTO CLASSMATE VALUES (0002, 'Петр', '22', 'Иваново');
+INSERT INTO CLASSMATE VALUES (0003, 'Анна', '35', 'Питер');
+INSERT INTO CLASSMATE VALUES (0004, 'Юлия', '18', 'Москва');
+INSERT INTO CLASSMATE VALUES (0005, 'Николай', '36', 'Москва');
